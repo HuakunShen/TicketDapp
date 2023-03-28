8a90198953d7df001c5689b7e83af6778d317ae4
Huakun Shen <huakun.shen@huakunshen.com>
2023-03-28 00:20:37 -0400

    Update contract

--- contracts/TicketSystem.sol
+++ contracts/TicketSystem.sol
@@ -6,8 +6,9 @@ contract TicketSystem {
 
   struct Event {
     uint id;
-    address seller;
-    uint nTickets;  // finite number of ticket in an event
+    address payable seller;
+    uint resellPriceMaxFactor; // ticket for resell can be sold at max resellPriceMaxFactor original price
+    bool returnable; // ticket can be returned to seller
   }
 
   enum TicketTier {Regular, VIP}
@@ -15,31 +16,102 @@ contract TicketSystem {
   struct Ticket {
     uint id;
     uint eventId;
-    address owner;
+    address payable owner;
+    uint originalPrice;
     uint price;
     TicketTier tier;
+    bool available; // owner of ticket is reselling it
   }
 
 
   uint public eventCount = 0;
   uint public ticketCount = 0;
 
-  mapping(uint => Ticket) public tickets;
-  mapping(uint => Event) public events;
+  Ticket [] public tickets;
+  Event [] public events;
 
   // create an event with msg sender and return event id
-  function createEvent(uint nTicket) public returns (uint) {
+  function createEvent(uint resellPriceMaxFactor, bool returnable) public returns (uint) {
     eventCount++;
-    Event memory evt = Event(eventCount, msg.sender, nTicket);
-    events[eventCount] = evt;
+    Event memory evt = Event(eventCount, payable(msg.sender), resellPriceMaxFactor, returnable);
+    events.push(evt);
     return eventCount;
   }
 
-  // create ticket of event and return ticket id
-  // function createTicket(uint eventId) public returns (uint) {
-  //   require(events.contains(eventId), "Event Doesn't Exist");
-  //   Event memory targetEvt = events[eventId];
+  // get all events
+  function getEvents() public view returns (Event [] memory) {
+    return events;
+  }
 
-  // }
+  // seller create tickets, make ticket available
+  function createTicket(uint eventId, uint price, TicketTier tier) public {
+    require(eventId <= eventCount, "Event not found");
+    Event storage evt = events[eventId];
+    require(msg.sender == evt.seller, "Only seller can create ticket");
+    ticketCount++;
+    Ticket memory ticket = Ticket(ticketCount, eventId, payable(msg.sender), price, price, tier, true);
+    tickets.push(ticket);
+  }
+
+  // anyone can buy a ticket, transfer money from msg.sender to ticket owner
+  function buyTicket(uint ticketId) public payable {
+    require(ticketId <= ticketCount, "Ticket not found");
+    Ticket storage ticket = tickets[ticketId];
+    require(ticket.available, "Ticket not available");
+    require(msg.value == ticket.price, "Incorrect payment amount");
+    ticket.owner.transfer(msg.value);
+    ticket.available = false;
+  }
+
+  function getTicketCountByTier(uint eventId, TicketTier tier) public view returns (uint) {
+    uint count = 0;
+    for (uint i = 1; i <= ticketCount; i++) {
+      Ticket storage ticket = tickets[i];
+      if (ticket.eventId == eventId && ticket.tier == tier) {
+        count++;
+      }
+    }
+    return count;
+  }
   
+  // return ticket to seller, only if ticket is returnable. Refund is the original price
+  function returnTicket(uint ticketId) public {
+    require(ticketId <= ticketCount, "Ticket not found");
+    Ticket storage ticket = tickets[ticketId];
+    require(msg.sender == ticket.owner, "Only ticket owner can return ticket");
+    Event storage evt = events[ticket.eventId];
+    require(evt.returnable, "Ticket not returnable");
+    evt.seller.transfer(ticket.originalPrice);
+    ticket.owner = evt.seller; // reset ticket owner to seller
+    ticket.price = ticket.originalPrice; // reset ticket listed price to original price
+    ticket.available = true; // ticket is still available for resell
+  }
+
+
+
+  function getTicketsForEvent(uint eventId, TicketTier tier) public view returns (Ticket [] memory) {
+    Ticket [] memory _tickets = new Ticket[](ticketCount);
+    uint count = 0;
+    for (uint i = 1; i <= ticketCount; i++) {
+      Ticket storage ticket = tickets[i];
+      if (ticket.eventId == eventId && ticket.tier == tier) {
+        _tickets[count] = ticket;
+        count++;
+      }
+    }
+    return _tickets;
+  }
+
+  // list ticket for resell, make it available
+  function resellTicket(uint ticketId, uint price) public {
+    require(ticketId <= ticketCount, "Ticket not found");
+    Ticket storage ticket = tickets[ticketId];
+    require(msg.sender == ticket.owner, "Only ticket owner can resell ticket");
+    Event storage evt = events[ticket.eventId];
+    require(price <= ticket.originalPrice * evt.resellPriceMaxFactor, "Resell price too high");
+    ticket.price = price;
+    ticket.available = true;
+  }
+  
+
 }
